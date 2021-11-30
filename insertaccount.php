0618d6bdfcbd2f1529dd0e6f24b242a3ae3aa5f7
reiminozawa <nozawa.reimi@gmail.com>
2021-11-30 20:52:54 +0900

    test version03

--- insertaccount.php
+++ insertaccount.php
@@ -2,7 +2,7 @@
 mb_internal_encoding("utf8");
 
 
-
+try{
 $pdo=new PDO("mysql:dbname=lesson1;host=localhost;","root","root"
             );
 
@@ -10,6 +10,9 @@ $pdo->exec("insert into
 diworks_account(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,delete_flag)values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".password_hash($_POST['password'], PASSWORD_DEFAULT)."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."',0);"
           );
 
+}catch(PDOException $e){
+    echo "エラー発生";
+}
 ?>
 
 
